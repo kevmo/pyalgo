c34bd6124e2057ff75832ea6942a8315f5e23d05
kevmo <k.james.moore@gmail.com>
2015-10-08 16:38:07 -0400

    Solved using bitwise operators

--- russian-peasant/src.py
+++ russian-peasant/src.py
@@ -1,4 +1,5 @@
 def russian_multiply(num1, num2):
+    """Verbose."""
     sorted_nums = sorted([num1, num2])
     low, high = sorted_nums[0], sorted_nums[1]
     low_array = []
@@ -17,3 +18,16 @@ def russian_multiply(num1, num2):
             result += high_array[n]
 
     return result
+
+
+    def bit_russian(num1, num2):
+        """Solved using bit shifting."""
+        x, y = num1, num2
+        z = 0
+
+        while x > 0:
+            if x % 2: z = z + y
+            x = x >> 1
+            y = y << 1
+
+        return z
